7d6ec9389d3b42817d2e9360addbb59ee8291e4d
dnrlrl <leewwo2o@naver.com>
2016-10-22 22:04:03 +0900

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,25 +1,19 @@
 public class Player{
   private String name=null;
-  private int sum=0;
-  private int first=0;
-  private int second=0;
+  int num=0;
   public Player(String _name){
-    name=_name;
+    name= _name;
   }
-  public void play(){
+  public int play(){
     Dice d=new Dice();
-    d.roll();
-    first=getNum();
-    System.out.println("one more please!"+first);
-    d.roll();
-    second=getNum();
-    System.out.println(second);
-    sum=first+second;
-  }
+    num=d.roll();
+    System.out.println(num);
+    return num;
+    }
   public String getName(){
     return this.name;
   }
   public int getNum(){
-    return this.sum;
+    return this.num;
   }
 }
