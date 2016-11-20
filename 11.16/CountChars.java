bf06131251e60a74830576a2237a7f4d0dc3f00e
dnrlrl <leewwo2o@naver.com>
2016-11-20 17:21:30 +0900

    Add files via upload

---
+++ 11.16/CountChars.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class CountChars{
+  static void countChar(String str){
+    char[] letters=str.toCharArray();
+    Map<String,Integer> wc=new HashMap<String,Integer>();
+    for(char c:letters){
+      String i=Character.toString(c);
+      if(wc.containsKey(i)){
+        wc.put(i,wc.get(i)+1);
+      }else{
+        wc.put(i,1);
+      }
+    }
+    System.out.println(wc);
+  }
+  public static void main(String[] args){
+    countChar("lim is best professor");
+  }
+}
